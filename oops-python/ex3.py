d54be3928863d031b06b98899c4837c06d2ec15c
swagat2001 <swagatmohanty2018@gmail.com>
2025-05-12 18:34:00 +0530

    'oops-operation'

--- oops-python/ex3.py
+++ oops-python/ex3.py
@@ -1,5 +1,14 @@
-def mul(a,b):
-    def add(a,b):
-        return(a+b)
-        return(a*b)
-print(mul(10,2))
+class MATH_FUN():
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+    
+    def ADD(self):
+        return(self.a+self.b)
+
+    def MUL(self):
+        add=self.ADD()
+        print(f'add:{add}')
+        return(self.a*self.b)
+mul = MATH_FUN(100,200).MUL()
+print(f'mul:{mul}') 
