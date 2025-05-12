6a8b1a42dfb0a1b315817e84a403f25bf592e8fe
swagat2001 <swagatmohanty2018@gmail.com>
2025-05-12 18:09:56 +0530

    'oops-concept'

---
+++ oops-python/ex1.py
@@ -0,0 +1,21 @@
+# Step-1 : first create a class
+# step-2: every function inside class write a word self
+# step-3: call the class
+# step-4:call the function using class
+class MATH_FUN():
+    def ADD(self,a,b):    
+        return(a+b) 
+
+    def MUL(self,a,b):
+        return(a*b)
+    
+# obj=MATH_FUN()
+# add = obj.ADD(10,20)
+# mul = obj.MUL(10,20)
+# print(f'add:{add}')
+# print(f'mul:{mul}')
+
+add = MATH_FUN().ADD(10,20)
+mul = MATH_FUN().MUL(10,20)
+print(f'add:{add}')
+print(f'mul:{mul}')
---
+++ oops-python/ex2.py
@@ -0,0 +1,16 @@
+class MATH_FUN():
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+        
+    def ADD(self):
+        return(self.a+self.b)
+    
+    def MUL(self):
+        return(self.a*self.b)
+    
+    def DIV(self):
+        return(self.a/self.b)
+    
+    def SUB(self):
+        return(self.a-self.b)
